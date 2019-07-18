16041dc20850bce4b96df3159255fdbf98b81567
prius0304 <2639035034@qq.com>
2019-07-18 11:05:01 +0800

    20190718

---
+++ COMManger/FormMain.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace COMManger
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.COMList = new System.Windows.Forms.ListView();
+            this.BT_Delete = new System.Windows.Forms.Button();
+            this.CB_Language = new System.Windows.Forms.ComboBox();
+            this.BT_Clear = new System.Windows.Forms.Button();
+            this.BT_Save = new System.Windows.Forms.Button();
+            this.ImageList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // COMList
+            // 
+            resources.ApplyResources(this.COMList, "COMList");
+            this.COMList.LargeImageList = this.ImageList;
+            this.COMList.Name = "COMList";
+            this.COMList.SmallImageList = this.ImageList;
+            this.COMList.TileSize = new System.Drawing.Size(120, 20);
+            this.COMList.UseCompatibleStateImageBehavior = false;
+            this.COMList.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // BT_Delete
+            // 
+            resources.ApplyResources(this.BT_Delete, "BT_Delete");
+            this.BT_Delete.Name = "BT_Delete";
+            this.BT_Delete.UseVisualStyleBackColor = true;
+            this.BT_Delete.Click += new System.EventHandler(this.BT_Delete_Click);
+            // 
+            // CB_Language
+            // 
+            resources.ApplyResources(this.CB_Language, "CB_Language");
+            this.CB_Language.FormattingEnabled = true;
+            this.CB_Language.Items.AddRange(new object[] {
+            resources.GetString("CB_Language.Items"),
+            resources.GetString("CB_Language.Items1")});
+            this.CB_Language.Name = "CB_Language";
+            this.CB_Language.SelectedIndexChanged += new System.EventHandler(this.CB_Language_SelectedIndexChanged);
+            // 
+            // BT_Clear
+            // 
+            resources.ApplyResources(this.BT_Clear, "BT_Clear");
+            this.BT_Clear.Name = "BT_Clear";
+            this.BT_Clear.UseVisualStyleBackColor = true;
+            this.BT_Clear.Click += new System.EventHandler(this.BT_Clear_Click);
+            // 
+            // BT_Save
+            // 
+            this.BT_Save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.BT_Save, "BT_Save");
+            this.BT_Save.Name = "BT_Save";
+            this.BT_Save.UseVisualStyleBackColor = false;
+            this.BT_Save.Click += new System.EventHandler(this.BT_Save_Click);
+            // 
+            // ImageList
+            // 
+            this.ImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImageList.ImageStream")));
+            this.ImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImageList.Images.SetKeyName(0, "Port.png");
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BT_Save);
+            this.Controls.Add(this.BT_Clear);
+            this.Controls.Add(this.CB_Language);
+            this.Controls.Add(this.BT_Delete);
+            this.Controls.Add(this.COMList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_Closing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView COMList;
+        private System.Windows.Forms.Button BT_Delete;
+        private System.Windows.Forms.ComboBox CB_Language;
+        private System.Windows.Forms.Button BT_Clear;
+        private System.Windows.Forms.Button BT_Save;
+        private System.Windows.Forms.ImageList ImageList;
+    }
+}
+
---
+++ COMManger/FormMain.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace COMManger
+{
+    public partial class FormMain : Form
+    {
+        RegistryKey Root;
+        RegistryKey Key;
+        byte[] OriginalCfg;
+        byte[] CurrentCfg;
+
+        byte[] BITList = new byte[]
+        {
+            1,
+            2,
+            4,
+            8,
+            16,
+            32,
+            64,
+            128,
+        };
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            Root = Registry.LocalMachine;
+            Key = Root.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\COM Name Arbiter", true);
+        }
+
+        private void ApplyResource()
+        {
+            ComponentResourceManager res = new ComponentResourceManager(typeof(FormMain));
+            foreach (Control ctl in Controls)
+            {
+                res.ApplyResources(ctl, ctl.Name);
+            }
+            this.Text = res.GetString("FormMain.Text");
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            res.ApplyResources(this, "$this");
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            OriginalCfg = (byte[])Key.GetValue("ComDB");
+            CurrentCfg = new byte[OriginalCfg.Length];
+            Array.Copy(OriginalCfg, 0, CurrentCfg, 0, OriginalCfg.Length);
+
+            RefreshList(CurrentCfg);
+
+            CB_Language.SelectedIndex = 0;
+        }
+
+        private void FormMain_Closing(object sender, EventArgs e)
+        {
+            if(!CurrentCfg.SequenceEqual(OriginalCfg))
+            {
+                ComponentResourceManager res = new ComponentResourceManager(typeof(FormMain));
+                if (MessageBox.Show(res.GetString("NoSaveAlert"), "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    BT_Save_Click(null, null);
+            }
+        }
+
+        private void RefreshList(byte[] argc)
+        {
+            COMList.Items.Clear();
+
+            for (int i = 0; i < argc.Length; i++)
+            {
+                for (int j = 0; j < BITList.Length; j++)
+                {
+                    if ((argc[i] & BITList[j]) == BITList[j])
+                        COMList.Items.Add($"COM{i * 8 + j + 1}", 0);
+                }
+            }
+        }
+
+        private void CB_Language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(CB_Language.SelectedIndex)
+            {
+                case 0:
+                    Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("zh-CN");
+                    break;
+                case 1:
+                    Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("EN");
+                    break;
+            }
+            
+            ApplyResource();
+        }
+
+        private void BT_Delete_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem item in COMList.SelectedItems)
+            {
+                int Index = Convert.ToInt32(item.Text.Remove(0, 3)) - 1;
+                int Block = Index / 8;
+                Index = Index % 8;
+                CurrentCfg[Block] = (byte)(CurrentCfg[Block] & ~BITList[Index]);
+            }
+
+            RefreshList(CurrentCfg);
+        }
+
+        private void BT_Clear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < CurrentCfg.Length; i++)
+                CurrentCfg[i] = 0;
+
+            RefreshList(CurrentCfg);
+        }
+
+        private void BT_Save_Click(object sender, EventArgs e)
+        {
+            ComponentResourceManager res = new ComponentResourceManager(typeof(FormMain));
+
+            try
+            {
+                Key.SetValue("COMDb", CurrentCfg);
+                Array.Copy(CurrentCfg, 0, OriginalCfg, 0, CurrentCfg.Length);
+                MessageBox.Show(res.GetString("SaveOK"));
+            }
+            catch(Exception)
+            {
+                MessageBox.Show(res.GetString("SaveFail"));
+            }
+        }
+    }
+}
